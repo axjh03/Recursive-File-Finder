3b98e57d6a1d43f2aebe32caf2d503cd81b71779
Alok Jha <117200589+axjh03@users.noreply.github.com>
2023-07-12 15:10:30 -0500

    python verison

--- finder.py
+++ finder.py
@@ -1,93 +1,67 @@
 import os
 import sys
 import time
-from tqdm import tqdm
-from treelib import Tree
 from colorama import Fore, Style, init
 
 # Initialize colorama
 init()
 
-def search_item_recursive(item_name, directory, tree):
-    count = 0  # Number of files/folders searched
-    copies = 0  # Number of copies found
-    locations = []  # Locations where the item is found
+num_dirs_searched = 0
+num_files_searched = 0
 
-    for root, dirs, files in os.walk(directory):
-        count += 1
-        if item_name in files or item_name in dirs:
-            location = os.path.join(root, item_name)
-            locations.append(location)
-            copies += 1
+def search_item_recursive(item_name, directory):
+    global num_dirs_searched, num_files_searched
+    found = False  # Flag to track if the item has been found
 
-    return locations, copies, count
+    for entry in os.scandir(directory):
+        if entry.name == item_name:
+            path = os.path.join(directory, entry.name)
+            print("Path:", path)
+            found = True
 
-def find_item_recursive(item_name, starting_directory, option):
+        if entry.is_dir() and not entry.name.startswith(('.', '..')):
+            num_dirs_searched += 1
+            found |= search_item_recursive(item_name, entry.path)
+        else:
+            num_files_searched += 1
+
+    return found
+
+def find_item_recursive(item_name, starting_directory):
     if not os.path.isdir(starting_directory):
         print("Invalid starting directory.")
         return
 
-    tree = Tree()
-    tree.create_node("/", "/")  # Add root node
-
     print(f"Searching in: {Fore.GREEN}{starting_directory}{Style.RESET_ALL}")
 
-    with tqdm(total=0, unit="directory", bar_format="{l_bar}{bar} [ {elapsed}<{remaining}, {rate_fmt}]") as pbar:
-        for root, dirs, files in os.walk(starting_directory):
-            parent_node = os.path.dirname(root)
-            if parent_node == starting_directory:
-                parent_node = "/"
+    start_time = time.time()
+    found = search_item_recursive(item_name, starting_directory)
+    end_time = time.time()
 
-            if not tree.contains(parent_node):
-                add_missing_parents(tree, parent_node)
+    elapsed_time = end_time - start_time
 
-            tree.create_node(os.path.basename(root), root, parent=parent_node)
+    if not found:
+        print("Item not found")
 
-            if option == "-file":
-                locations, copies, count = search_item_recursive(item_name, root, tree)
-                if copies > 0:
-                    print(f"\n{Fore.YELLOW}Found {copies} copy(s) of '{item_name}' at the following location(s):")
-                    for location in locations:
-                        print(location)
-                    break
-            elif option == "-folder":
-                if item_name in dirs:
-                    result = os.path.join(root, item_name)
-                    print(f"\n{Fore.YELLOW}Found: {result}{Style.RESET_ALL}")
-                    break
+    print(f"\nTotal directories searched: {num_dirs_searched}")
+    print(f"Total files searched: {num_files_searched}")
+    print(f"Time taken: {elapsed_time:.2f} seconds")
 
-            pbar.total += len(dirs)
-            pbar.update()
-
-    print(tree.show(line_type="ascii-em"))
-
-    if option == "-file" and copies == 0:
-        print(f"{Fore.RED}No copies of '{item_name}' found.{Style.RESET_ALL}")
-
-    print(f"\nSearch completed in {int(time.time() - pbar.start_t)} seconds")
-    print(f"Total number of files and folders searched: {pbar.total + 1}")
-
-def add_missing_parents(tree, directory):
-    if directory == "/":
-        return
-
-    parent = os.path.dirname(directory)
-    if not tree.contains(directory):
-        add_missing_parents(tree, parent)
-        tree.create_node(os.path.basename(directory), directory, parent=parent)
+def get_current_directory():
+    return os.getcwd()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3 or len(sys.argv) > 4:
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
         print("Invalid number of arguments.")
-        print("Usage: python finder.py [option] item_name [starting_directory]")
+        print("Usage: python finder.py -file filename.ext OR python finder.py -folder foldername [starting_directory]")
         sys.exit(1)
 
     option = sys.argv[1]
     item_name = sys.argv[2]
-    starting_directory = sys.argv[3] if len(sys.argv) == 4 else os.getcwd()
+    starting_directory = sys.argv[3] if len(sys.argv) == 4 else get_current_directory()
 
     if option == "-file" or option == "-folder":
-        find_item_recursive(item_name, starting_directory, option)
+        find_item_recursive(item_name, starting_directory)
     else:
         print("Invalid option.")
-        print("Usage: python finder.py [option] item_name [starting_directory]")
+        print("Usage: python finder.py -file filename.ext OR python finder.py -folder foldername [starting_directory]")
